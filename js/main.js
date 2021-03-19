e64f1c50e7c86c7e4ecba4a69c3a98c7471fffa8
Athos Ciuldin <32936797+aciuldin@users.noreply.github.com>
2021-03-18 22:38:31 -0300

    insercao do trailer e de informacoes no botao mais informacoes

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function redireciona(){
+    window.open("https://www.youtube.com/watch?v=SvSkxBYuoQY");
+
+}
+
+   document.getElementById('mensagem').onclick = function(){
+    swal("O congressista Francis Underwood e sua mulher, Claire, fazem de tudo para conquistar seus objetivos, não importa o que aconteça. Um mundo político recheado de ganância, corrupção e luxúria na capital Washington.");
+  };
